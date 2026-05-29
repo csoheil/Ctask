4ce0e30011537ae934bc0eed41eb717c9739eccf
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2026-05-29 11:47:37 +0330

    feat(contact): add responsive contact page styling

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Contact - CTask</title>
-
+    <link rel="stylesheet" href="assets/css/contact.css">
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
