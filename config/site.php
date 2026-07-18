b809442c6bfdd1b01e682b63da3511ac1a0ac576
soheil mohammadi <soheilmohammadi0o19@gmail.com>
2026-07-18 20:57:45 +0330

    feat(config): centralize site metadata and contact information

---
+++ config/site.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General
+    |--------------------------------------------------------------------------
+    */
+
+    'name' => 'CTask',
+
+    'tagline' => 'We Do Your Tasks',
+
+    'description' => 'Affordable programming, research writing, design, presentation, video editing, and industrial design services.',
+
+    'keywords' => implode(', ', [
+        'CTask',
+        'Programming',
+        'PHP',
+        'Web Development',
+        'Research Writing',
+        'PowerPoint',
+        'PDF',
+        'Video Editing',
+        'Logo Design',
+        'CATIA',
+        'Industrial Design'
+    ]),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Contact
+    |--------------------------------------------------------------------------
+    */
+
+    'email' => 'ctask2026@gmail.com',
+
+    'website' => 'http://localhost/ctask',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Social
+    |--------------------------------------------------------------------------
+    */
+
+    'github' => '',
+
+    'linkedin' => '',
+
+    'instagram' => '',
+
+];
